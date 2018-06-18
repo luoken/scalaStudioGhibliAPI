f6472df9fd3a34c4aac823e1767e12baceddb40b
Ken Luo <luozhirong@gmail.com>
2018-06-18 15:13:43 -0700

    test throws error

--- build.sbt
+++ build.sbt
@@ -6,6 +6,10 @@ version := "0.1-SNAPSHOT"
 
 scalaVersion := "2.11.8"
 
+libraryDependencies += "com.lihaoyi" %%% "scalatags" % "0.6.2"
+libraryDependencies += "com.lihaoyi" %%% "utest" % "0.6.3" % "test"
+testFrameworks += new TestFramework("utest.runner.Framework")
+
 libraryDependencies ++= Seq(
   "org.scala-js" %%% "scalajs-dom" % "0.9.1",
   "com.lihaoyi" %%% "scalatags" % "0.6.1"
--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -1,44 +1,31 @@
 package example
 import scala.scalajs.js.annotation.JSExport
 import org.scalajs.dom
+import dom.html
 import org.scalajs.dom.html
 import scala.util.Random
+import scala.scalajs.js
+import scalatags.JsDom.all._
 
-case class Point(x: Int, y: Int){
-  def +(p: Point) = Point(x + p.x, y + p.y)
-  def /(d: Int) = Point(x / d, y / d)
-}
 
 @JSExport
-object ScalaJSExample {
+object ScalaJSExample extends{
   @JSExport
-  def main(canvas: html.Canvas): Unit = {
-    val ctx = canvas.getContext("2d")
-                    .asInstanceOf[dom.CanvasRenderingContext2D]
+  def main(target: html.Div) = {
+    import dom.ext._
+    import scala.scalajs.concurrent.JSExecutionContext.Implicits.runNow
 
-    var count = 0
-    var p = Point(0, 0)
-    val corners = Seq(Point(255, 255), Point(0, 255), Point(128, 0))
+    val url = "https://ghibliapi.herokuapp.com/films"
 
-    def clear() = {
-      ctx.fillStyle = "black"
-      ctx.fillRect(0, 0, 255, 255)
+    Ajax.get(url).onSuccess{ case xhr =>
+      target.appendChild(
+        pre(
+          js.JSON.stringify(
+            js.JSON.parse(xhr.responseText),
+            space=4
+          )
+        ).render
+      )
     }
-
-    def run = for (i <- 0 until 10){
-      if (count % 3000 == 0) clear()
-      count += 1
-      p = (p + corners(Random.nextInt(3))) / 2
-
-      val height = 512.0 / (255 + p.y)
-      val r = (p.x * height).toInt
-      val g = ((255-p.x) * height).toInt
-      val b = p.y
-      ctx.fillStyle = s"rgb($g, $r, $b)"
-
-      ctx.fillRect(p.x, p.y, 1, 1)
-    }
-
-    dom.window.setInterval(() => run, 50)
   }
 }
